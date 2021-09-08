9a29828eb25e016d53a04aa53e96d51777bf08de
tronexis <tronexis>
2021-09-09 01:57:20 +0600

    added mysql config

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { createConnection } from "mysql2";
 import { config } from "dotenv";
-import employeeRoutes from './routes/employee';
-import cors from 'cors'
+import employeeRoutes from "./routes/employee";
+import cors from "cors";
 
 config();
 
 const app = express();
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = process.env.HOST;
-const port = process.env.PORT;
-const database = process.env.DB_NAME;
+const user = process.env.DB_USER || "udjmetxswhgojqea";
+const password = process.env.DB_PASSWORD || "KuuHBjvOwsJdOkiIu1S3";
+const host =
+  process.env.HOST || "bjlcwrkvhgrmtsyqmx5h-mysql.services.clever-cloud.com";
+const port = process.env.PORT || 4000;
+const database = process.env.DB_NAME || "bjlcwrkvhgrmtsyqmx5h";
 export const db = createConnection({
   user,
   password,
@@ -26,7 +27,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/', employeeRoutes);
+app.use("/", employeeRoutes);
 
 app.use("/", (req, res) => {
   res.status(200).send("Hello World");
